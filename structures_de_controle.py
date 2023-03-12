1bb089193230011ef57fd8d4882b20d54452e69e
Alban <renier.alban@gmail.com>
2023-03-11 22:26:47 -0400

    les fonctions et les structures

---
+++ structures_de_controle.py
@@ -0,0 +1,18 @@
+# if-else
+x = 10
+if x > 0:
+    print("x est positif")
+elif x == 0:
+    print("x est nul")
+else:
+    print("x est négatif")
+
+# Boucle for
+for i in range(5):
+    print(i)
+
+# Boucle while
+i = 0
+while i < 5:
+    print(i)
+    i += 1
