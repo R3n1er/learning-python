b4565c4e6a2983cf7eb5dce11fb68646729a533d
Alban <renier.alban@gmail.com>
2023-03-13 15:21:34 -0400

    initial.py

---
+++ initials.py
@@ -0,0 +1,26 @@
+# First Name and Last Name
+# Alban RENIER
+
+
+# A fun fact about yourself
+# I am learning Python3
+
+# Output off first initaial
+print("  A  " )
+print(" A  A ")
+print("A    A")
+print("AAAAAA")
+print("A    A")
+print("A    A")
+print("A    A")
+
+name ="""
+RRRR
+R    R
+R    R
+RRRR
+R R
+R  R
+R    R
+"""
+print(name)
