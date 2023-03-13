bdb32f96e6fe6850bb18e2cb0b6aee4415889ec4
Alban <renier.alban@gmail.com>
2023-03-12 21:29:00 -0400

    deocuverte des fonctions

--- fonctions.py
+++ fonctions.py
@@ -12,21 +12,17 @@ def addition(a, b):
 # Appel de fonction avec retour de valeur
 resultat = addition(5, 3)
 print("5 + 3 =", resultat)
-# if-else
-x = 10
-if x > 0:
-    print("x est positif")
-elif x == 0:
-    print("x est nul")
-else:
-    print("x est négatif")
+# Fonction simple
+def dire_bonjour(nom):
+    print("Bonjour", nom)
 
-# Boucle for
-for i in range(5):
-    print(i)
+# Appel de fonction
+dire_bonjour("Alice")
 
-# Boucle while
-i = 0
-while i < 5:
-    print(i)
-    i += 1
+# Fonction avec retour de valeur
+def addition(a, b):
+    return a + b
+
+# Appel de fonction avec retour de valeur
+resultat = addition(5, 3)
+print("5 + 3 =", resultat)
---
+++ structures.py
@@ -0,0 +1,14 @@
+# Fonction simple
+def dire_bonjour(nom):
+    print("Bonjour", nom)
+
+# Appel de fonction
+dire_bonjour("Alice")
+
+# Fonction avec retour de valeur
+def addition(a, b):
+    return a + b
+
+# Appel de fonction avec retour de valeur
+resultat = addition(5, 3)
+print("5 + 3 =", resultat)
