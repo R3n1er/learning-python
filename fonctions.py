b5551658927b39291929c93d5d25c62b940d2d1b
Alban <renier.alban@gmail.com>
2023-03-12 21:27:45 -0400

    structures.py

--- fonctions.py
+++ fonctions.py
@@ -12,3 +12,21 @@ def addition(a, b):
 # Appel de fonction avec retour de valeur
 resultat = addition(5, 3)
 print("5 + 3 =", resultat)
+# if-else
+x = 10
+if x > 0:
+    print("x est positif")
+elif x == 0:
+    print("x est nul")
+else:
+    print("x est négatif")
+
+# Boucle for
+for i in range(5):
+    print(i)
+
+# Boucle while
+i = 0
+while i < 5:
+    print(i)
+    i += 1
