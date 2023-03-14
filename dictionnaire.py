564c11d8d833f8ecefddedeccc791a17c2566256
Alban <renier.alban@gmail.com>
2023-03-14 08:19:56 -0400

    dictionnaire en python

---
+++ dictionnaire.py
@@ -0,0 +1,10 @@
+#Déclaration d'un dictionnaire
+infos_labradoodle = {
+    "poids" : "13 à 16 kg",
+    "origine" : "Etats-Unis"
+}
+
+#Ajouter une nouvelle clé-valeur au dictionnaire
+infos_labradoodle['nom_scientifique'] = "Canis lupus familiaris"
+
+print(infos_labradoodle)
