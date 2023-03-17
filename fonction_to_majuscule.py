31b3d556f2e24f107aa5c823b0723c594d90fa06
Alban <renier.alban@gmail.com>
2023-03-17 08:50:02 -0400

    fonction majuscule

---
+++ fonction_to_majuscule.py
@@ -0,0 +1,11 @@
+#Définition de la fonction
+def majuscules():
+    chaine = input("Entrez une chaîne de caractères : ")
+    # un prompt demande à l'utilisateur de saisir sa phrase
+    return chaine.upper()
+
+
+# Utilisation de la fonction
+resultat = majuscules()
+print(resultat)
+
