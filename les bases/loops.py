d261913f90a4733bf3994b82507b968882e1cc64
Alban <renier.alban@gmail.com>
2023-03-19 10:26:27 -0400

    travail avec les loops ou boucles

---
+++ les bases/loops.py
@@ -0,0 +1,17 @@
+#Boucle qui liste des nombres dans un range
+for i in range(6):
+    print(i)
+
+
+#Boucle qui donne une liste de noms
+names=["Harry", "Hermione", "Ron"]
+
+for name in names:
+    print(name)
+
+# Bouche qui affiche les caractère d'une string
+
+name = "Harry"
+
+for character in name:
+    print(character)
