d864f771ba2222acd77ac41174f91e7f83efc0e1
Alban <renier.alban@gmail.com>
2023-03-18 22:28:25 -0400

    sequence and data structures

---
+++ les bases/sequences.py
@@ -0,0 +1,16 @@
+name = input("Your Name: ")
+
+print(name[1]) #Affiche le second caractère
+
+
+names = ["Harry", "Ron", "Henry"]
+print(names)
+
+print(names[1])
+
+#Data Structures in Python
+
+#list
+#tuple -- sequence of immutables values
+#set
+#dict -- collection of key-value pairs
