6e684db672546129531f0625f518f198ad736d0c
Alban <renier.alban@gmail.com>
2023-03-19 10:32:49 -0400

    dictionnaries

---
+++ les bases/dictionnaries.py
@@ -0,0 +1,10 @@
+# Créer un nouveau dictionnaire
+
+houses = {"Harry" : "Gryffindor", "Draco":"Slytherin"}
+
+#Ajouter un élément au dictionnaire
+houses["Hermione"] = "Gryffindor"
+
+print(houses["Harry"])
+
+print(houses["Hermione"])
