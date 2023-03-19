743f31df0ad419aae1bdf63e2738fdceba967ec2
Alban <renier.alban@gmail.com>
2023-03-19 10:19:56 -0400

    utilisation de set

--- les bases/sets.py
+++ les bases/sets.py
@@ -7,3 +7,18 @@
 s = set()
 
 # Add elements to set
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(5)
+s.add(3)
+
+s.remove(3)
+s.remove(5)
+
+print(s)
+
+# Imprimer dans une string formaté
+
+print(f"The set has {len(s)} elements.")
