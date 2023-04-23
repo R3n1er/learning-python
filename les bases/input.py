727262d119886eb288c61b46482e34a19bf0f189
Alban RENIER <renier.alban@gmail.com>
2023-04-23 09:05:30 -0300

    input.py

---
+++ les bases/input.py
@@ -0,0 +1,6 @@
+
+prenom = input("Entree votre prénom: ")
+ville_de_naissance = input("Entrez votre ville de naissance: ")
+age = input("Quel est votre age? ")
+
+print(prenom + " " + ville_de_naissance + " " + age)
--- les bases/type.py
+++ les bases/type.py
@@ -10,3 +10,10 @@ nombre = 15
 resultat = "Le nombre est " + str(nombre) #avec concatenation
 
 print(resultat)
+
+
+a = 2
+b = 6
+c = 3
+
+resultat = str(a) + "+" + str(b) + "+" + str(c)
