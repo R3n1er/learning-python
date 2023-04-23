34aeb361ee4973d1c41e0bbdacdc96e6349a8b8f
Alban RENIER <renier.alban@gmail.com>
2023-04-23 08:32:28 -0300

    travailler avec les types

---
+++ les bases/type.py
@@ -0,0 +1,11 @@
+# Afficher le type d'une variable
+nombre = input('Entrez un nombre : ')
+print(nombre)
+
+print(type(nombre))
+
+# Convertir une variable d'un type a un autre
+
+nombre = 15
+resultat = "Le nombre est " + str(nombre) #avec concatenation
+print(resultat)
