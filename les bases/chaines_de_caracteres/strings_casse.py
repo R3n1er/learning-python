7d8eda49f6f920e5bdbb3c97f535cb2fe5e58bfc
Alban <renier.alban@gmail.com>
2023-04-23 21:38:15 -0300

    changer la casse en python sur une string

---
+++ les bases/chaines_de_caracteres/strings_casse.py
@@ -0,0 +1,9 @@
+# Methode pour changer la casse des carteres
+print("> Utilisation de la fonction upper()")
+print("Bonjour".upper())
+print("> utilisation de la fonction lower()")
+print("Bonjour".lower())
+print("> Utilisation de la fonction capitalize()")
+print("bonjour tous le monde".capitalize())
+print("utilisation de la fonction title()")
+print("bonjour tous le monde".title())
