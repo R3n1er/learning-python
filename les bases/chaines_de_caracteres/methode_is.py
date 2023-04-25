57fd5608e630adfd1ec103eae58c9916f1d23813
Alban RENIER <renier.alban@gmail.com>
2023-04-25 07:17:03 -0300

    methode is

---
+++ les bases/chaines_de_caracteres/methode_is.py
@@ -0,0 +1,11 @@
+
+#utilisation de la methode islower()
+print("bonjour".islower())
+
+print("Bonjour".islower())
+
+#utilisation de la methode isdigit()
+
+print("50".isdigit())
+
+print("a50".isdigit())
