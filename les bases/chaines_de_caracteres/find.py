565191e514698d8ad4d5aac1013391e177c57bf8
Alban RENIER <renier.alban@gmail.com>
2023-04-25 09:34:09 -0300

    methode find et index

---
+++ les bases/chaines_de_caracteres/find.py
@@ -0,0 +1,11 @@
+print("Bonjour le jour".find("jour"))
+
+print("Bonjour le jour".index("jour"))
+
+print("Bonjour le jour".find("soir"))
+
+
+#Methode pour chercher à partir de la fin (droite)
+
+
+print("Bonjour le jour".rfind("jour"))
