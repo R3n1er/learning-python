29ca885a0fd21cca22b205728fbd1d833231c545
Alban RENIER <renier.alban@gmail.com>
2023-04-25 17:10:02 -0300

    utilisation de la methode format

---
+++ les bases/strings_format/format.py
@@ -0,0 +1,13 @@
+# Pour les versions de python inférieur à 3.6
+
+#Utilisation de la methode format
+
+age = 26
+
+phrase = "J'ai {} ans".format(age)
+
+print(phrase)
+
+phrase = "J'ai {a} ans".format(a=age)
+
+print(phrase)
