7e5266db6ac24806c689525e220cae115ac94567
Alban RENIER <renier.alban@gmail.com>
2023-05-07 09:46:16 -0300

    fonction random ex01

---
+++ les bases/random/ex01.py
@@ -0,0 +1,13 @@
+import random
+a = random.randint(1,30)
+print(f"a = {a}")
+
+b = random.randint(1,30)
+print(f"b = {b}")
+
+if a > b :
+    print("Le nombre a est plus grand que le nombre b.")
+elif b > a :
+    print("Le nombre b est plus grand que le nombre a.")
+elif a == b
+    print("Le nombre a et le nombre b sont égaux.")
