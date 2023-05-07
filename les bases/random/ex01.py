239746a68ff5b8f44fa872dffacf145a0ead3cb3
Alban RENIER <renier.alban@gmail.com>
2023-05-07 09:46:49 -0300

    modification erreur elif

--- les bases/random/ex01.py
+++ les bases/random/ex01.py
@@ -9,5 +9,5 @@ if a > b :
     print("Le nombre a est plus grand que le nombre b.")
 elif b > a :
     print("Le nombre b est plus grand que le nombre a.")
-elif a == b
+elif a == b :
     print("Le nombre a et le nombre b sont égaux.")
