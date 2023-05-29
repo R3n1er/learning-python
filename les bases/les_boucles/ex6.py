c45519ef05daf7999ecbe17859a2b0882eb0c50a
Alban RENIER <renier.alban@gmail.com>
2023-05-28 21:54:34 -0300

    exercice sur les boucle avec range

---
+++ les bases/les_boucles/ex6.py
@@ -0,0 +1,10 @@
+# Le script doit afficher 10 utilisateurs de ma manière suivante
+
+# Utilisateur 1
+# Utilisateur 2
+# Utilisateur 3
+
+#######
+for i in range(1,11) :
+    utilisateur = "Utilisateur " + str(i)
+    print(utilisateur)
