a2f4526eabcf46ef2b44a2d5e7b38ea61bc487e7
Alban RENIER <renier.alban@gmail.com>
2023-06-10 18:30:28 -0300

    exercie sur les boucles while

---
+++ les_bases/les_boucles/ex08.py
@@ -0,0 +1,8 @@
+#Sortir d'une boucle While avec input
+
+continuer = "o"
+while continuer == "o":
+    print("On continue !")
+    continuer = input("Voulez-vous continuer ? o/n ")
+
+print("Boucle terminée !")
