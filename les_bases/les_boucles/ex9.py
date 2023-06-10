66166bb344e044b4f82acac17f8d9265a6aae617
Alban RENIER <renier.alban@gmail.com>
2023-06-10 18:32:17 -0300

    exercice sur les comprehension de listes

--- les_bases/les_boucles/ex9.py
+++ les_bases/les_boucles/ex9.py
@@ -1,38 +1,25 @@
+# Filtrer les nombres pairs
 nombres = [1, 21, 5, 44, 4, 9, 5, 83, 29, 31, 25, 38]
-nombres_pairs = []
-for i in nombres:
-    if i % 2 == 0:
-        nombres_pairs.append(i)
-
+nombres_pairs = [i for i in nombres if i % 2 == 0]
 print(nombres_pairs)
 
 # ---------------------------------------------------- #
 
+# Filtrer les nombres positifs
 nombres = range(-10, 10)
-nombres_positifs = []
-for i in nombres:
-    if i >= 0:
-        nombres_positifs.append(i)
-
+nombres_positifs = [i for i in nombres if i >= 0]
 print(nombres_positifs)
 
 # ---------------------------------------------------- #
 
+# Modifier les nombres en les doublant
 nombres = range(5)
-nombres_doubles = []
-for i in nombres:
-    nombres_doubles.append(i * 2)
-
+nombres_doubles = [i * 2 for i in nombres]
 print(nombres_doubles)
 
 # ---------------------------------------------------- #
 
+# Modifier les nombres en inversant les pairs et en mettant des négatifs pour les impairs
 nombres = range(10)
-nombres_inverses = []
-for i in nombres:
-    if i % 2 == 0:
-        nombres_inverses.append(i)
-    else:
-        nombres_inverses.append(-i)
-
+nombres_inverses = [i if i % 2 == 0 else -i for i in nombres]
 print(nombres_inverses)
